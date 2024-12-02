3df1528f98800671ad6470961367c817e5087bee
Sneha Bisht <149566925+techwithsnehabisht@users.noreply.github.com>
2024-12-02 09:45:10 +0530

    Add files via upload

---
+++ Grocery/grcscript.js
@@ -0,0 +1,87 @@
+// Selectors
+let searchForm = document.querySelector('.search-form');
+let shoppingCart = document.querySelector('.shopping-cart');
+let loginForm = document.querySelector('.login-form');
+let navbar = document.querySelector('.navbar');
+
+// Search Form Toggle
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+};
+
+// Shopping Cart Toggle
+document.querySelector('#cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+};
+
+// Login Form Toggle
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+};
+
+// Navbar Toggle
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+};
+
+// Close All Components on Scroll
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+};
+
+// Initialize Swiper for Product Slider
+var productSwiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+// Initialize Swiper for Review Slider
+var reviewSwiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
